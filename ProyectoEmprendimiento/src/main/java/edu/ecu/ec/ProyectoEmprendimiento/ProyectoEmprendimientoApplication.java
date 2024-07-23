8aeff4dceb397d665c7c541a12502c2984ee5669
JordiFPA <167142190+JordiFPA@users.noreply.github.com>
2024-07-23 16:40:03 -0500

    Las facturas bien

--- ProyectoEmprendimiento/src/main/java/edu/ecu/ec/ProyectoEmprendimiento/ProyectoEmprendimientoApplication.java
+++ ProyectoEmprendimiento/src/main/java/edu/ecu/ec/ProyectoEmprendimiento/ProyectoEmprendimientoApplication.java
@@ -1,7 +1,6 @@
 package edu.ecu.ec.ProyectoEmprendimiento;
 
 import edu.ecu.ec.ProyectoEmprendimiento.View.Login;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
